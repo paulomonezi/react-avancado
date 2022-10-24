1c4cdf160c55b69449b053c020fce20e7fa61a7f
Paulo Monezi <paulo.monezi@icloud.com>
2022-10-24 09:26:24 -0400

    lifecycle using hooks

--- src/components/deck-of-cards/deck-of-cards.js
+++ src/components/deck-of-cards/deck-of-cards.js
@@ -1,4 +1,6 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
+
+
 
 async function createDeck() {
     const response = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
@@ -11,43 +13,75 @@ async function getCards(deckId) {
     return await response.json()
 }
 
-class DeckOfCards extends Component {
-    constructor() {
-        console.log('constructor')
-        super()
-        this.state = {
-            cards: []
+const DeckOfCards = () => {
+
+    const [deck, setDeck] = useState({
+        cards: []
+    })
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const deckId = await createDeck()
+            const data = await getCards(deckId)
+
+            setDeck({
+                cards: data.cards
+            })
         }
-    }
-
-    async componentDidMount() {
-        console.log('didmount')
-        const deckId = await createDeck()
-        const data = await getCards(deckId)
-
-        this.setState({
-            cards: data.cards
-        })
-    }
-
-    render() {
-        console.log('render')
-        return (
-            <section>
-                <ul>
-                    {
-                        this.state.cards.map((card, index) => {
-                            return(
-                                <li key={index}>
-                                    <img src={card.image} alt={card.value} />
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
-            </section>
-        )
-    }
+        fetchData()
+    }, [])
+    return (
+        <section>
+            <ul>
+                {
+                    deck.cards.map((card, index) => {
+                        return (
+                            <li key={index}>
+                                <img src={card.image} alt={card.value} />
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </section>
+    )
 }
+// constructor() {
+//     console.log('constructor')
+//     super()
+//     this.state = {
+//         cards: []
+//     }
+// }
+
+// async componentDidMount() {
+//     console.log('didmount')
+//     const deckId = await createDeck()
+//     const data = await getCards(deckId)
+
+//     this.setState({
+//         cards: data.cards
+//     })
+// }
+
+// render() {
+//     console.log('render')
+//     return (
+//         <section>
+//             <ul>
+//                 {
+//                     this.state.cards.map((card, index) => {
+//                         return(
+//                             <li key={index}>
+//                                 <img src={card.image} alt={card.value} />
+//                             </li>
+//                         )
+//                     })
+//                 }
+//             </ul>
+//         </section>
+//     )
+// }
+
 
 export default DeckOfCards
